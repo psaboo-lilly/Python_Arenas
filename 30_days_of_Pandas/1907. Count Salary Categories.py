c66b3828179b2443c0ff2b9769848648842fc13e
3055917 (parthsaboo) <157591927+psaboo-lilly@users.noreply.github.com>
2026-05-07 01:37:23 +0530

    Add function to count salary categories in accounts
    Implement a function to categorize bank accounts based on income and count them in each category.

---
+++ 30_days_of_Pandas/1907. Count Salary Categories.py
@@ -0,0 +1,66 @@
+'''
+Table: Accounts
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| account_id  | int  |
+| income      | int  |
++-------------+------+
+account_id is the primary key (column with unique values) for this table.
+Each row contains information about the monthly income for one bank account.
+ 
+
+Write a solution to calculate the number of bank accounts for each salary category. The salary categories are:
+
+"Low Salary": All the salaries strictly less than $20000.
+"Average Salary": All the salaries in the inclusive range [$20000, $50000].
+"High Salary": All the salaries strictly greater than $50000.
+The result table must contain all three categories. If there are no accounts in a category, return 0.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Accounts table:
++------------+--------+
+| account_id | income |
++------------+--------+
+| 3          | 108939 |
+| 2          | 12747  |
+| 8          | 87709  |
+| 6          | 91796  |
++------------+--------+
+Output: 
++----------------+----------------+
+| category       | accounts_count |
++----------------+----------------+
+| Low Salary     | 1              |
+| Average Salary | 0              |
+| High Salary    | 3              |
++----------------+----------------+
+'''
+
+#--------SOLUTION--------#
+
+import pandas as pd
+
+def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
+    
+    #calculating counts and storing in variables
+    low_salary_accts = accounts[accounts['income'] < 20000]['account_id'].nunique()
+    avg_salary_accts = accounts[(accounts['income'] >= 20000) & (accounts['income'] <= 50000)]['account_id'].nunique()
+    high_salary_accts = accounts[accounts['income'] > 50000]['account_id'].nunique()
+
+    #creating a new dataframe with new fields
+    df = pd.DataFrame(
+        {
+            'category': ['Low Salary', 'Average Salary', 'High Salary'],
+            'accounts_count': [low_salary_accts, avg_salary_accts, high_salary_accts]
+        }
+    )
+
+    return df
